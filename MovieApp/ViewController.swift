9fbebf53766426ebea5ca5f2bd2dab550e401a22
Jeel1803 <jeel.khadsaliya1803@gmail.com>
2022-10-29 12:27:20 +1300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Jeel Khadsaliya on 29/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
